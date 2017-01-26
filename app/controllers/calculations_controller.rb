f48ec60c46de0fae359a5835be8bc94fc6896a57
cmyoder <cmyoder@uchicago.edu>
2017-01-25 18:01:14 -0600

    forms and stuff

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,36 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @num = params["the_number"].to_f
+
+    @solution = @num ** 2
+
+    render("calculations/square.html.erb")
+  end
+
+  def random
+
+    @minimum = params["min"].to_i
+    @maximum = params["max"].to_i
+    @random = rand(@minimum .. @maximum)
+
+    render("calculations/random.html.erb")
+  end
+
+  def sqrt_form
+
+    render("calculations/sqrt_form.html.erb")
+  end
+
+  def sqrt
+
+    number = params["numb"].to_f
+
+    @solution = number ** 0.5
+
+    render("calculations/sqrt.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:the_number", { :controller => "calculations", :action => "square" })
+
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+
+  get("/square_root_form", { :controller => "calculations", :action => "sqrt_form" })
+
+  get("/calculate_square_root", { :controller => "calculations", :action => "sqrt"})
+
 end
